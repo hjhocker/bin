34678385defb923e9b89fee9100b00095987a526
Harrison Hocker <hjhocker@gmail.com>
2015-12-18 23:30:39 -0600

    updated weather script

--- weather.py
+++ weather.py
@@ -22,15 +22,18 @@ proxies = {}
 body = json.dumps({})
 header = {}
 auth = HTTPBasicAuth('fake@example.com', 'not_a_real_password')
-url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22neworleans%2C%20la%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-response = requests.get(url, headers={}, data=body, proxies=proxies, auth=auth, verify=True, stream=False)
+nola = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22neworleans%2C%20la%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+houston = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22houston%2C%20tx%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+response = requests.get(nola, headers={}, data=body, proxies=proxies, auth=auth, verify=True, stream=False)
 
 if response.ok:
     response_headers = response.headers
     data = json.loads(response.content)
     atmosphere = data['query']['results']['channel']['atmosphere']
-    forecast = data['query']['results']['channel']['item']['forecast'][1]
+    forecast = data['query']['results']['channel']['item']['forecast'][2]
+    condition = data['query']['results']['channel']['item']['condition']
+    print bcolors.HEADER  + forecast['date'] + bcolors.ENDC
     print bcolors.OKGREEN + "Today is going to be " + forecast['text'] + bcolors.ENDC
     print bcolors.WARNING + "The high is " + bcolors.RED + forecast['high'] + bcolors.WARNING + " and the low is " + bcolors.OKBLUE + forecast['low'] + bcolors.ENDC
-
+    print bcolors.WARNING + "The current temperature is " + bcolors.CYAN + condition['temp'] + bcolors.ENDC
 exit
